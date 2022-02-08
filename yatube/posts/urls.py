b9da4072b85e18b994258346c42af96b67e882e8
Ilya Ilin <ilyn.i.p@gmail.com>
2022-02-09 00:16:08 +0300

    Исправлены замечания ревьюера + замечание теста

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,5 +6,5 @@ app_name = "group"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/slug:<slug>/", views.group_posts, name="group_list"),
+    path("group/<slug:slug>/", views.group_posts, name="group_list"),
 ]
