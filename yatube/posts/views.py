15b2c9c4c0740a0d60a7d6451af4fe19e36a5746
Ilya Ilin <ilyn.i.p@gmail.com>
2022-02-08 23:59:07 +0300

    Исправлены замечания ревьюера

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,16 +5,26 @@ from .models import Post, Group
 
 def index(request):
     template = "posts/index.html"
-    posts = Post.objects.order_by("-pub_date")[:10]
+    number_of_posts = 10
+    posts = Post.objects.order_by("-pub_date")[:number_of_posts]
     context = {
         "posts": posts,
     }
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_plural_name = "Посты"
+        ordering = ["-pub_date"]
+
     return render(request, template, context)
 
 
 def group_posts(request, slug):
+    number_of_posts = 10
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[
+        :number_of_posts
+    ]
     context = {
         "group": group,
         "posts": posts,
